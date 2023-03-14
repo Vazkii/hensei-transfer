9077745ec0f06c58b31b677d316ffa90b4c73f89
Vasco Lavos <vazkii@hotmail.com>
2023-03-14 15:58:21 +0000

    export characters

--- export.js
+++ export.js
@@ -4,7 +4,32 @@ function __export_hensei(deck) {
     var out = {};
     out['name'] = name;
     out['class'] = deck['pc']['job']['master']['name'];
-    console.log(out);
+
+    var subskills = [];
+    var set_action = deck['pc']['set_action'];
+    for (var i = set_action.length - 1; i >= 0; i--) {
+        var obj = set_action[i];
+        subskills.push(obj['name']);
+    }
+    out['subskills'] = subskills;
+    
+    var characters = [];
+    var npc = deck['npc'];
+    for(k in npc) {
+        var charOut = {};
+        var obj = npc[k];
+        var master = obj['master'];
+        var param = obj['param'];
+        
+        console.log(master['name']);
+        charOut['name'] = master['name'];
+        charOut['id'] = master['id'];
+        charOut['ringed'] = param['has_npcaugment_constant'];
+        charOut['uncap'] = param['evolution'];
+
+        characters.push(charOut);
+    }
+    out['characters'] = characters;
     
     alert(JSON.stringify(out));
 }
