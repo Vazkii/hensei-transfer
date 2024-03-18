c076c79782623468fc19c48546abfe0d5e180965
Justin Edmund <justin@jedmund.com>
2024-03-17 19:20:05 -0700

    Update aio.js

--- incl/aio.js
+++ incl/aio.js
@@ -3,8 +3,8 @@
 		//GBF-TO-JSON
 		__hensei_export(Game, function(str) {
 			__hensei_clipboard_copy(str);
-			if(confirm('Copied team data to clipboard! If you press OK, a new tab in app.granblue.team will be open now - click the bookmark again on it and paste your data in there.'))
-            	window.open('https://app.granblue.team', '_blank');
+			if(confirm('Copied team data to clipboard! If you press OK, a new tab in granblue.team will be open now - click the bookmark again on it and paste your data in there.'))
+            	window.open('https://granblue.team', '_blank');
 		});
 	}
 	function __json_to_hensei() {
@@ -13,9 +13,9 @@
 
 	//VERSION-CHECK
 
-	if(window.location.href.includes('app.granblue.team'))
+	if(window.location.href.includes('granblue.team'))
 		__json_to_hensei();
 	else if(window.location.href.includes('game.granbluefantasy.jp'))
 		__gbf_to_json();
-	else alert('Can only be used in game.granbluefantasy.jp or app.granblue.team');
+	else alert('Can only be used in game.granbluefantasy.jp or granblue.team');
 })();
